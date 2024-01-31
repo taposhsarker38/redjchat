f423bf338b41a25cd663095114a787c1d8f8d219
taposhsarker38 <taposhsarker38@gmail.com>
2024-01-31 23:59:39 +0600

    validation

--- server/models.py
+++ server/models.py
@@ -1,10 +1,33 @@
 from collections.abc import Iterable
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+from .validators import validate_icon_size
 # Create your models here.
+def server_banner_upload_path(instance,filename):
+    return f"server/{instance.id}/server_banner/{filename}"
+def server_icon_upload_path(instance,filename):
+    return f"server/{instance.id}/server_icons/{filename}"
+def category_icon_upload_path(instance,filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, blank=True, null=True)
+    def save(self,*args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category,id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category,self).save(*args, **kwargs)
+    @receiver(models.signals.pre_delete,sender="server.Category")
+    def category_delete_files(sender,instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name =="icon":
+                file = getattr(instance,field.name)
+                if file:
+                    file.delete(save=False)
     def __str__(self):
         return self.name
 
@@ -22,8 +45,24 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
-    def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel,self).save(*args, **kwargs)
+    banner = models.ImageField(upload_to=server_banner_upload_path, null=True,blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path, null=True,blank=True,validators=[validate_icon_size])
+    def save(self,*args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category,id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Category,self).save(*args, **kwargs)
+    @receiver(models.signals.pre_delete,sender="server.Server")
+    def category_delete_files(sender,instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name =="icon" or field.name=="banner":
+                file = getattr(instance,field.name)
+                if file:
+                    file.delete(save=False)
+    def __str__(self):
+        return self.name
     def __str__(self):
         return self.name
--- server/views.py
+++ server/views.py
@@ -6,14 +6,75 @@ from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from .models import Server
 from .serializer import ServerSerializer
 from django.db.models import Count
-
+from .schema import server_list_docs
 # Define a Django ViewSet for handling Server-related operations
 class ServerListViewSet(viewsets.ViewSet):
     # Set the initial queryset to include all Server objects
     queryset = Server.objects.all()
-
+    
     # Define a method to handle the 'list' action (GET request)
+    @server_list_docs
     def list(self, request):
+        """
+    Lists servers based on provided query parameters.
+
+    This method retrieves a list of servers based on various query parameters
+    that can be specified in the HTTP request. It supports filtering by category,
+    limiting the number of servers returned, filtering by user, and more.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Raises:
+        AuthenticationFailed: If 'by_user' or 'by_serverid' is specified and
+            the user is not authenticated.
+
+    Filters:
+        - category (str): Filters servers by category name.
+        - qty (str): Limits the number of servers returned.
+        - by_user (str, optional): Filters servers by the requesting user.
+        - by_serverid (str, optional): Filters servers by the specified server ID.
+        - with_num_members (str, optional): Includes the count of members in each server.
+
+    Returns:
+        Response: A JSON response containing a list of serialized servers.
+
+    Usage:
+        To list servers:
+        ```
+        GET /api/servers/?category=gaming&qty=5&by_user=true&with_num_members=true
+        ```
+
+    Note:
+        - If 'by_user' is specified, authentication is required.
+        - If 'by_serverid' is specified, the server ID must exist in the queryset.
+
+    Raises:
+        ValidationError: If there is an issue with the provided server ID.
+            - "Server with id {by_serverid} not found"
+            - "Server value error"
+
+    Examples:
+        - To retrieve gaming servers with details and member count:
+        ```
+        GET /api/servers/?category=gaming&with_num_members=true
+        ```
+
+        - To retrieve the first 3 servers for the authenticated user:
+        ```
+        GET /api/servers/?qty=3&by_user=true
+        ```
+
+        - To retrieve a specific server by ID:
+        ```
+        GET /api/servers/?by_serverid=123
+        ```
+
+        - To list all servers without any filtering:
+        ```
+        GET /api/servers/
+        ```
+    """
         # Retrieve query parameters from the request
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
@@ -22,15 +83,18 @@ class ServerListViewSet(viewsets.ViewSet):
         with_num_members = request.query_params.get("with_num_members") == "true"
 
         # Check authentication if 'by_user' or 'by_serverid' is specified
-        if by_user or by_serverid and not request.user.is_authenticated:
-            raise AuthenticationFailed()
+        # if by_user or by_serverid and not request.user.is_authenticated:
+        #     raise AuthenticationFailed()
 
         # Apply filters based on query parameters
         if category:
             self.queryset = self.queryset.filter(category__name=category)
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
         if qty:
